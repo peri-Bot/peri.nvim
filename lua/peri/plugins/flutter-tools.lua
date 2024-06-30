e0e5561d8a2c1cd8cae5a35a1e8db1f4bccf7ba7
Kidus Abebe <kidsebk@gmail.com>
2024-06-30 22:26:42 +0300

    conifg restructuring

---
+++ lua/peri/plugins/flutter-tools.lua
@@ -0,0 +1,38 @@
+return {
+  'akinsho/flutter-tools.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'stevearc/dressing.nvim',
+  },
+  config = function()
+    require('flutter-tools').setup {
+      flutter_path = '/home/peribot/Downloads/flutter/bin/flutter',
+      debugger = {
+        -- make these two params true to enable debug mode
+        enabled = false,
+        run_via_dap = false,
+        register_configurations = function(_)
+          require('dap').adapters.dart = {
+            type = 'executable',
+            command = vim.fn.stdpath 'data' .. '/mason/bin/dart-debug-adapter',
+            args = { 'flutter' },
+          }
+
+          require('dap').configurations.dart = {
+            {
+              type = 'dart',
+              request = 'launch',
+              name = 'Launch flutter',
+              dartSdkPath = 'home/Downloads/flutter/bin/cache/dart-sdk/',
+              flutterSdkPath = 'home/Downloads/flutter',
+              program = '${workspaceFolder}/lib/main.dart',
+              cwd = '${workspaceFolder}',
+            },
+          }
+          -- uncomment below line if you've launch.json file already in your vscode setup
+          -- require("dap.ext.vscode").load_launchjs()
+        end,
+      },
+    }
+  end,
+}
--- lua/peri/plugins/init.lua
+++ lua/peri/plugins/init.lua
@@ -1,44 +1,27 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 return {
-  {
-    'akinsho/flutter-tools.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'stevearc/dressing.nvim',
-    },
-    config = function()
-      require('flutter-tools').setup {
-        flutter_path = '/home/peribot/Downloads/flutter/bin/flutter',
-        debugger = {
-          -- make these two params true to enable debug mode
-          enabled = false,
-          run_via_dap = false,
-          register_configurations = function(_)
-            require('dap').adapters.dart = {
-              type = 'executable',
-              command = vim.fn.stdpath 'data' .. '/mason/bin/dart-debug-adapter',
-              args = { 'flutter' },
-            }
 
-            require('dap').configurations.dart = {
-              {
-                type = 'dart',
-                request = 'launch',
-                name = 'Launch flutter',
-                dartSdkPath = 'home/Downloads/flutter/bin/cache/dart-sdk/',
-                flutterSdkPath = 'home/Downloads/flutter',
-                program = '${workspaceFolder}/lib/main.dart',
-                cwd = '${workspaceFolder}',
-              },
-            }
-            -- uncomment below line if you've launch.json file already in your vscode setup
-            -- require("dap.ext.vscode").load_launchjs()
-          end,
-        },
-      }
-    end,
+  {
+    'nvim-lua/plenary.nvim',
+    name = 'plenary',
   },
+
+  'eandrju/cellular-automaton.nvim',
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 }
