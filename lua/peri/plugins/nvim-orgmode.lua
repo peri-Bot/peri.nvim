078bb8b22dc8ff3962c0a624f66972e0c5387e70
Kidus Abebe <kidsebk@gmail.com>
2024-11-14 13:17:27 +0300

    fix: timestamp min rounding up/down

--- lua/peri/plugins/nvim-orgmode.lua
+++ lua/peri/plugins/nvim-orgmode.lua
@@ -8,6 +8,7 @@ return {
       org_agenda_files = '~/orgfiles/**/*',
       org_default_notes_file = '~/orgfiles/refile.org',
       org_hide_leading_stars = true,
+      org_calendar_round_minutes = false,
     }
 
     -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
