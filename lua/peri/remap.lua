5c7316cc3609288f99aecb5c4ef81205dca204d4
Kidus Abebe <kidsebk@gmail.com>
2024-11-14 12:10:04 +0300

    add: new keymaps

--- lua/peri/remap.lua
+++ lua/peri/remap.lua
@@ -29,3 +29,8 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz', { desc = ' Next error' })
+vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz', { desc = ' Next error' })
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz', { desc = ' Next error' })
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz', { desc = ' Next error' })
